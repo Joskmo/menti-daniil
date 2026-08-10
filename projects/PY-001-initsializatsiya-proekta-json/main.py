40e45afa1e65dbd974db4d4e6457ed81482275da
Joskmo <zinkin.ar@gmail.com>
2026-08-10 13:09:26 +0300

    chore: initialized project

--- projects/PY-001-initsializatsiya-proekta-json/main.py
+++ projects/PY-001-initsializatsiya-proekta-json/main.py
@@ -1,1 +1,25 @@
-'PY-001: Инициализация проекта JSON.'
+import json
+
+with open(r"C:/Users/Даниил/Desktop/proga/py/json parse practice/test.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+last_id = data[-1]['id']
+nextid = last_id + 1
+
+name = str(input("Введите ваше имя: "))
+last_name = str(input("Введите вашу фамилию: "))
+
+print("Добавление данных!")
+
+new_User = {
+    "id": nextid,
+    "first_name": name,
+    "last_name": last_name
+}
+
+data.append(new_User)
+
+with open(r"C:/Users/Даниил/Desktop/proga/py/json parse practice/test.json", 'w', encoding="utf-8") as file:
+    json.dump(data, file, ensure_ascii=False, indent=2)
+
+print(f"Данные успешно добавлены! Ваш id: {nextid}")
